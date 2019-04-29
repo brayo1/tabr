fc67d7a0c24fe4b999d22a55e368a76c8f1cb5be
brayo1 <43237596+brayo1@users.noreply.github.com>
2019-04-29 17:47:49 +0300

    static folder

---
+++ static/js/chat.js
@@ -0,0 +1,13 @@
+
+// CHAT BOOT MESSENGER////////////////////////
+var $
+$(document).ready(function () {
+  $('.chat_on').click(function () {
+    $('.Layout').toggle()
+    $('.chat_on').hide(300)
+  })
+  $('.chat_close_icon').click(function () {
+    $('.Layout').hide()
+    $('.chat_on').show(300)
+  })
+})
